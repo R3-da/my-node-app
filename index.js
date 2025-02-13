50202318151ad035e38313ce1a8ad973f15ee2f5
r3-da <haddan.reda.net@gmail.com>
2025-02-13 12:39:03 +0100

    Update product IDs in the products array in index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000;
 const products = [
     { id: 1, name: 'product1' },
     { id: 2, name: 'product2' },
-    { id: 2, name: 'product3' },
-    { id: 2, name: 'product4' },
-    { id: 2, name: 'product5' }
+    { id: 3, name: 'product3' },
+    { id: 4, name: 'product4' },
+    { id: 5, name: 'product5' }
 ];
 
 app.get('/api/products', (req, res) => {
